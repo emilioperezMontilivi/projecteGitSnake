fb650c4ab43279c584f712fecaf6c112bd1a7514
Emilio Pérez <admin@gmail.com>
2020-11-27 03:50:01 +0100

    Commit procedim a iniciar codi JocSnake.cs

--- ProjecteSnake/JocSnake.cs
+++ ProjecteSnake/JocSnake.cs
@@ -3,10 +3,88 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace ProjecteSnake
 {
     class JocSnake
     {
+        public const int WIDTH = 5;
+        public const int HEIGHT = 5;
+        public const int NPOMES = 3;
+
+        private Point cap;
+        DireccioSnake direccio;
+        private List<Point> pomes;
+
+        public JocSnake()
+        {
+            cap = new Point(0, 0);
+            pomes = new List<Point>();
+            //distribuir les pomes
+            Random r = new Random();
+            for (int i = 0; i < NPOMES; i++)
+            {
+                var x = r.Next(0, WIDTH - 1);
+                var y = r.Next(0, HEIGHT - 1);
+                while (x == 0 && y == 0)
+                {
+                    x = r.Next(0, WIDTH - 1);
+                    y = r.Next(0, HEIGHT - 1);
+                }
+
+                pomes.Add(new Point(x, y));
+            }
+
+        }
+
+        public DireccioSnake Direccio { get => direccio; set => direccio = value; }
+        public Point Cap { get => cap; set => cap = value; }
+        public List<Point> Pomes { get => pomes; set => pomes = value; }
+
+        public void moure()
+        {
+            if (direccio == DireccioSnake.Nord)
+            {
+                cap.Y--;
+                if (cap.Y < 0)
+                {
+                    cap.Y = HEIGHT - 1;
+                }
+
+            }
+            else if (direccio == DireccioSnake.Sud)
+            {
+                cap.Y++;
+                if (cap.Y == HEIGHT)
+                {
+                    cap.Y = 0;
+                }
+            }
+            else if (direccio == DireccioSnake.Est)
+            {
+                cap.X++;
+                if (cap.X == WIDTH)
+                {
+                    cap.X = 0;
+                }
+            }
+            else
+            {
+                cap.X--;
+                if (cap.X < 0)
+                {
+                    cap.X = WIDTH;
+                }
+            }
+        }
+
+    }
+    public enum DireccioSnake
+    {
+        Nord,
+        Sud,
+        Est,
+        Oest
     }
 }
--- ProjecteSnake/MainWindow.xaml.cs
+++ ProjecteSnake/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace ProjecteSnake
 {
@@ -20,9 +21,76 @@ namespace ProjecteSnake
     /// </summary>
     public partial class MainWindow : Window
     {
+        public const int SNAKE_HEAD_SIZE_WIDTH = 35;
+        public const int SNAKE_HEAD_SIZE_HEIGHT = 35;
+        JocSnake joc;
+        DispatcherTimer timer;
+        SolidColorBrush brushSnake = new SolidColorBrush(Colors.Green);
+        SolidColorBrush brushPoma = new SolidColorBrush(Colors.Red);
         public MainWindow()
         {
             InitializeComponent();
+            InitializeComponent();
+            this.KeyDown += GameWindow_KeyDown;
+            joc = new JocSnake();
+            timer = new DispatcherTimer();
+            timer.Tick += Timer_Tick;
+            timer.Interval = TimeSpan.FromMilliseconds(500);
+        }
+
+        private void GameWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Up)
+            {
+                joc.Direccio = DireccioSnake.Nord;
+            }
+            else if (e.Key == Key.Down)
+            {
+                joc.Direccio = DireccioSnake.Sud;
+            }
+            else if (e.Key == Key.Left)
+            {
+                joc.Direccio = DireccioSnake.Oest;
+            }
+            else
+            {
+                joc.Direccio = DireccioSnake.Est;
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            //Pintar
+            canvas.Children.Clear();
+            Ellipse elSnake = new Ellipse()
+            {
+                Fill = brushSnake,
+                Width = SNAKE_HEAD_SIZE_WIDTH,
+                Height = SNAKE_HEAD_SIZE_HEIGHT,
+            };
+            Canvas.SetTop(elSnake, joc.Cap.Y * SNAKE_HEAD_SIZE_HEIGHT);
+            Canvas.SetLeft(elSnake, joc.Cap.X * SNAKE_HEAD_SIZE_HEIGHT);
+            canvas.Children.Add(elSnake);
+
+            foreach (var poma in joc.Pomes)
+            {
+                Ellipse elpoma = new Ellipse()
+                {
+                    Fill = brushPoma,
+                    Width = SNAKE_HEAD_SIZE_WIDTH,
+                    Height = SNAKE_HEAD_SIZE_HEIGHT,
+                };
+                Canvas.SetTop(elpoma, poma.Y * SNAKE_HEAD_SIZE_HEIGHT);
+                Canvas.SetLeft(elpoma, poma.X * SNAKE_HEAD_SIZE_HEIGHT);
+                canvas.Children.Add(elpoma);
+            }
+
+            joc.moure();
+        }
+
+        private void btnIniciaJoc_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Start();
         }
     }
 }
